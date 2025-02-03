ca81850d7f9cbe1a7dc14afdbf29c3d8440101e4
Chidozie <chidough14@gmail.com>
2025-02-03 12:39:09 +0100

    Projects table with sorting functionality

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -3,10 +3,12 @@ import SelectInput from "@/Components/SelectInput";
 import TextInput from "@/Components/TextInput";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constans";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout"
-import { Head, Link, usePage } from "@inertiajs/react";
+import { Head, Link, router, usePage } from "@inertiajs/react";
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid'
+import TableHeading from "@/Components/TableHeading";
 
 
-const Index = ({projects, queryParams=null}) => {
+const Index = ({ projects, queryParams = null }) => {
   queryParams = queryParams || {}
   const user = usePage().props.auth.user;
 
@@ -16,12 +18,29 @@ const Index = ({projects, queryParams=null}) => {
     } else {
       delete queryParams[name]
     }
+
+    router.get(route('project.index'), queryParams)
   }
 
   const onKeyPress = (name, e) => {
     if (e.key !== 'Enter') return
 
     searchFieldChange(name, e.target.value)
+  }
+
+  const sortChanged = (name) => {
+    if (name === queryParams.sort_field) {
+      if (queryParams.sort_direction === 'asc') {
+        queryParams.sort_direction = 'desc'
+      } else {
+        queryParams.sort_direction = 'asc'
+      }
+    } else {
+      queryParams.sort_field = name
+      queryParams.sort_direction = 'asc'
+    }
+
+    router.get(route('project.index'), queryParams)
   }
 
   return (
@@ -33,7 +52,7 @@ const Index = ({projects, queryParams=null}) => {
         </h2>
       }
     >
-      <Head title="Dashboard" />
+      <Head title="Project" />
 
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -41,53 +60,76 @@ const Index = ({projects, queryParams=null}) => {
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <div className="relative overflow-x-auto">
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                      <th scope="col" className="px-6 py-3">
-                        ID
-                      </th>
-                      <th scope="col" className="px-6 py-3">
+                      <TableHeading
+                        name="id"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >id</TableHeading>
+
+                      <th className="px-3 py-3">
                         Image
                       </th>
-                      <th scope="col" className="px-6 py-3">
-                       Name
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                       Status
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Create Date
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Due Date
-                      </th>
-                      <th scope="col" className="px-6 py-3">
+
+                      <TableHeading
+                        name="name"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >name</TableHeading>
+                      
+                      <TableHeading
+                        name="status"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >status</TableHeading>
+
+                       <TableHeading
+                        name="created_at"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >Create Date</TableHeading>
+
+                      <TableHeading
+                        name="due_date"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >Due Date</TableHeading>
+
+                      <th className="px-3 py-3">
                         Created By
                       </th>
-                      <th scope="col" className="px-6 py-3 text-right">
-                       Actions
+                      <th className="px-3 py-3 text-right">
+                        Actions
                       </th>
                     </tr>
                   </thead>
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                       <th scope="col" className="px-6 py-3">
-                        
+
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        
+
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        <TextInput 
-                          className="w-full" 
-                          placeholder="Project Name" 
+                        <TextInput
+                          className="w-full"
+                          placeholder="Project Name"
+                          defaultValue={queryParams.name}
                           onBlur={(e) => searchFieldChange('name', e.target.value)}
                           onKeyPress={(e) => onKeyPress('name', e)}
                         />
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        <SelectInput 
-                          className="w-full" 
+                        <SelectInput
+                          className="w-full"
+                          defaultValue={queryParams.status}
                           onChange={(e) => searchFieldChange("status", e.target.value)}
                         >
                           <option value="">Select Status</option>
@@ -97,16 +139,16 @@ const Index = ({projects, queryParams=null}) => {
                         </SelectInput>
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        
+
                       </th>
                       <th scope="col" className="px-6 py-3">
-                        
+
                       </th>
                       <th scope="col" className="px-6 py-3">
-                      
+
                       </th>
                       <th scope="col" className="px-6 py-3 text-right">
-                       
+
                       </th>
                     </tr>
                   </thead>
@@ -118,16 +160,16 @@ const Index = ({projects, queryParams=null}) => {
                             {project.id}
                           </th>
                           <td className="px-3 py-2">
-                            <img src={project.image_path} alt="" style={{width: 60}} />
+                            <img src={project.image_path} alt="" style={{ width: 60 }} />
                           </td>
                           <td className="px-3 py-2">
                             {project.name}
                           </td>
                           <td className="px-3 py-2">
-                            <span 
+                            <span
                               className={
-                              "px-2 py-1 rounded text-white " +
-                              PROJECT_STATUS_CLASS_MAP[project.status]
+                                "px-2 py-1 rounded text-white " +
+                                PROJECT_STATUS_CLASS_MAP[project.status]
                               }
                             >
                               {PROJECT_STATUS_TEXT_MAP[project.status]}
@@ -144,11 +186,11 @@ const Index = ({projects, queryParams=null}) => {
                           </td>
                           <td className="px-3 py-2">
                             <Link href={route('project.edit', project.id)} className='text-blue-500'>
-                                Edit
+                              Edit
                             </Link>
 
                             <Link href={route('project.destroy', project.id)} className='text-red-500 ml-3'>
-                                Delete
+                              Delete
                             </Link>
                           </td>
                         </tr>
